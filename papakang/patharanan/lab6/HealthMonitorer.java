562723afa20c43e3daea56536d9dd86753b4b681
Patharanan <96046535+Patharanan@users.noreply.github.com>
2022-02-15 00:11:05 +0700

    Delete papakang/patharanan/lab6 directory

--- papakang/patharanan/lab6/HealthMonitorer.java
+++
@@ -1,6 +0,0 @@
-package papakang.patharanan.lab6;
-
-// Creating a public interface called HealthMonitorer that has a method called displayHeartRates.
-public interface HealthMonitorer {
-  public void displayHeartRates();
-}
