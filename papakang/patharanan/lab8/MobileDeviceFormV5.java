72bb6b26dbaf7e1c955a02d4a55aa3c442f7738e
Patharanan <96046535+Patharanan@users.noreply.github.com>
2022-03-14 14:20:50 +0700

    Add files via upload
    Updated lab7 and lab8 because I typed the word "from" as "form".

---
+++ papakang/patharanan/lab8/MobileDeviceFormV5.java
@@ -0,0 +1,102 @@
+/**Implement the program called MobileDeviceFormV5  which extends from MobileDeviceFormV4
+ * In this program  will set the font and color of components such that the program
+ * Set the values of text fields and the text area
+ * @Auther Patharanan Papakang
+ * @Version 1 25,Feb 2022
+ */
+package papakang.patharanan.lab8;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class MobileDeviceFormV5 extends MobileDeviceFormV4 {
+
+  // Creating two instance variables of the Font class.
+  protected Font font14Plain, font14Bold;
+
+  // This is the constructor of the class. It calls the constructor of the superclass.
+  public MobileDeviceFormV5(String titel) {
+    super(titel);
+  }
+
+  /**
+   * this method is call method setFormValues and method setFrontsColors.
+   */
+  protected void addComponents() {
+    super.addComponents();
+    this.setFormValues();
+    this.setFontsColors();
+  }
+
+  /**
+   * This method set the form values of txtBrand,txtModel,txtColor,txtWeight,txtPrice
+   * Set text of reviewTxtArea.
+   * Set JRadiobutton to IOSJRB.
+   * Set typeComboBox to Tablet
+   */
+  protected void setFormValues() {
+    txtBrand.setText("Apple");
+    txtModel.setText("iPad Air");
+    txtColor.setText("Silver");
+    txtWeight.setText("0.469");
+    txtPrice.setText("24,900.00");
+
+    reviewTxtArea.setText(
+      "The iPad Air is Apple's mid-range tablet and the current model features a new design with flat edges, the A14 Bionic chip,a USB-C port, Touch ID, a range of color options, and more."
+    );
+
+    iOSJRB.setSelected(true);
+    typeComboBox.setSelectedItem("Tablet");
+  }
+
+  /**
+   * Set the fonts  text fields
+   * Set color of button.
+   */
+  protected void setFontsColors() {
+    // This is creating a new font object with the name "SansSerif", the font style is bold, and the
+    // font size is 14.
+    font14Bold = new Font("SansSerif", Font.BOLD, 14);
+    txtBrand.setFont(font14Bold);
+    txtModel.setFont(font14Bold);
+    txtColor.setFont(font14Bold);
+    txtWeight.setFont(font14Bold);
+    txtPrice.setFont(font14Bold);
+    reviewTxtArea.setFont(font14Bold);
+
+    // This is creating a new font object with the name "SansSerif", the font style is plain, and the
+    // font size is 14.
+    font14Plain = new Font("SansSerif", Font.PLAIN, 14);
+    brandNameLabel.setFont(font14Plain);
+    modelNameLabel.setFont(font14Plain);
+    colorLabel.setFont(font14Plain);
+    weightLabel.setFont(font14Plain);
+    priceLabel.setFont(font14Plain);
+    mobileLabel.setFont(font14Plain);
+    typeLabel.setFont(font14Plain);
+    reviewLabel.setFont(font14Plain);
+
+    // This is setting the color cancelButton for red and okBUtton for blue.
+    cancelButton.setForeground(Color.RED);
+    okButton.setForeground(Color.BLUE);
+  }
+
+  public static void createAndShowGUI() {
+    MobileDeviceFormV5 mobileDeviceFormV5 = new MobileDeviceFormV5(
+      "Mobile Device Form V5"
+    );
+    mobileDeviceFormV5.addComponents();
+    mobileDeviceFormV5.addMenus();
+    mobileDeviceFormV5.setFrameFeatures();
+  }
+
+  public static void main(String[] args) {
+    SwingUtilities.invokeLater(
+      new Runnable() {
+        public void run() {
+          createAndShowGUI();
+        }
+      }
+    );
+  }
+}
---
+++ papakang/patharanan/lab8/MobileDeviceFormV6.java
@@ -0,0 +1,124 @@
+/**This program implement the program called MobileDeviceFormV6  which extends from MobileDeviceFormV5
+ * Add a  panel that contains the label “Features:”
+ * Add a panel that extends from JPanel that draws an image
+ * @Auther Patharanan Papakang
+ * @Version 1 27,Feb 2022
+ */
+package papakang.patharanan.lab8;
+
+import java.awt.*;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.JPanel;
+
+public class MobileDeviceFormV6 extends MobileDeviceFormV5 {
+
+  // Creating a panel that contains the image.
+  protected JPanel drawPanel, featuresPanel;
+  // Creating a label called featuresLabel and set it to protected.
+  protected JLabel featuresLabel;
+  // This is creating a list of features.
+  protected JList featuresList;
+  // Creating an array of strings.
+  protected String[] featuresArray;
+
+  public MobileDeviceFormV6(String titel) {
+    super(titel);
+  }
+
+  /**
+   * This Method is creating an array of strings add list to the features panel.
+   *  Add the features panel to the total panel
+   * Set font
+   */
+  @Override
+  protected void addComponents() {
+    super.addComponents();
+    featuresLabel = new JLabel("Features:");
+    featuresPanel = new JPanel();
+    featuresPanel.setLayout(new BorderLayout());
+
+    // Creating an array of strings.
+    featuresArray =
+      new String[] {
+        "10.9-inch display",
+        "12MP rear camera",
+        "next-generation Touch ID sensor",
+      };
+    featuresList = new JList(featuresArray);
+
+    featuresPanel.add(featuresList);
+
+    featuresLabel.setFont(font14Plain);
+    featuresList.setFont(font14Bold);
+
+    ReadImages content = new ReadImages();
+
+    featuresPanel.add(featuresLabel, BorderLayout.NORTH);
+
+    featuresPanel.add(content, BorderLayout.SOUTH);
+    totalPanel.add(featuresPanel, BorderLayout.SOUTH);
+  }
+
+  /**
+   *  Add a panel that extends from JPanel that draws an image read from the specified file.
+   */
+  public class ReadImages extends JPanel {
+
+    BufferedImage img;
+
+    public void paint(Graphics graphics) {
+      graphics.drawImage(img, 0, 0, null);
+    }
+
+    // A constructor that read the image from the specified file.
+
+    public ReadImages() {
+      try {
+        URL myImg = this.getClass().getResource("/images/ipadair.jpg");
+        img = ImageIO.read(myImg);
+      } catch (IOException e) {
+        e.printStackTrace(System.err);
+      }
+    }
+
+    /**
+     * Returns the preferred size of the image
+     *
+     * @return The preferred size of the image.
+     */
+    public Dimension getPreferredSize() {
+      if (img == null) {
+        return new Dimension(100, 100);
+      } else {
+        return new Dimension(img.getWidth() + 20, img.getHeight() + 60);
+      }
+    }
+  }
+
+  /**
+   * Create a new instance of the MobileDeviceFromV6 class and show it
+   */
+  public static void createAndShowGUI() {
+    MobileDeviceFormV6 mobileDeviceFormV6 = new MobileDeviceFormV6(
+      "Mobile Device Form V6"
+    );
+    mobileDeviceFormV6.addComponents();
+    mobileDeviceFormV6.addMenus();
+    mobileDeviceFormV6.setFrameFeatures();
+  }
+
+  public static void main(String[] args) {
+    SwingUtilities.invokeLater(
+      new Runnable() {
+        public void run() {
+          createAndShowGUI();
+        }
+      }
+    );
+  }
+}
