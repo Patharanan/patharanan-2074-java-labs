f67b060d66e484d58a2fcdc79be5b17df4a02a3d
Patharanan <96046535+Patharanan@users.noreply.github.com>
2022-03-14 14:05:50 +0700

    Add files via upload
    Updated lab7 and lab8 because I typed the word "from "as "form".

---
+++ papakang/patharanan/lab7/MobileDeviceFormV2.java
@@ -0,0 +1,95 @@
+/** MobileDeviceFromV2 extends  MobileDeviceFromV1 class.
+ * This program creating a new JComboBox object
+ * Add the label “Type:” and the combo box of device type which includes “Phone”, “Tablet”, “Smart Watch”
+ * and allow the user to edit this combo box.
+ * Add the label “Review:” and the text area
+ * @Auther Patharanan Papakang
+ * @Version 1 21,Feb 2022
+ */
+package papakang.patharanan.lab7;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class MobileDeviceFormV2 extends MobileDeviceFormV1 {
+
+  protected JPanel centerPanel, reviewPanel, typePanel;
+  protected JLabel typeLabel, reviewLabel;
+  protected JComboBox typeComboBox;
+  protected JTextArea reviewTxtArea;
+  protected JScrollPane scrollPane;
+
+  public MobileDeviceFormV2(String titel) {
+    super(titel);
+  }
+
+  /**
+   * This method creating a new JLabel object and setting the text to "Type:"
+   * and the combo box of device type which includes “Phone”, “Tablet”, “Smart Watch”.
+   * Creating a new JPanel object.Creating a new JComboBox object
+   * Creating a JTextArea object with 5 rows and 30 columns and  adding a text to the reviewTxtArea.
+   */
+
+  @Override
+  protected void addComponents() {
+    super.addComponents();
+
+    typeLabel = new JLabel("Type:");
+    typePanel = new JPanel();
+    typeComboBox = new JComboBox();
+    centerPanel = new JPanel();
+    centerPanel.setLayout(new BorderLayout()); // Setting the layout of the center panel to a BorderLayout.
+
+    typeComboBox.addItem("Phone");
+    typeComboBox.addItem("Tablet");
+    typeComboBox.addItem("Smart Watch");
+    typeComboBox.setSelectedIndex(1); //setting the selected index to 1.
+    typeComboBox.setEditable(true);
+
+    typePanel.setLayout(new GridLayout(1, 2)); //setting the layout to 1 rows 2 colums.
+
+    // Adding the typeLabel and typeComboBox to the typePanel.
+    typePanel.add(typeLabel);
+    typePanel.add(typeComboBox);
+
+    reviewLabel = new JLabel("Review:"); // Creating a new JLabel object and setting the text to "Review:".
+    reviewPanel = new JPanel();
+    reviewTxtArea = new JTextArea(5, 30);
+    reviewTxtArea.setLineWrap(true);
+    reviewTxtArea.setWrapStyleWord(true);
+    reviewTxtArea.setText(
+      "The iPhone 13’s brighter display, longer battery life and powerful cameras make it the best iPhone for the money. But we wish it had faster charging and the iPhone 13 Pro’s adaptive 120Hz display."
+    );
+
+    reviewPanel.setLayout(new BorderLayout());
+    // This is adding the reviewLabel and reviewTxtArea to the reviewPanel.
+    reviewPanel.add(reviewLabel, BorderLayout.PAGE_START);
+
+    scrollPane = new JScrollPane(reviewTxtArea);
+    reviewPanel.add(scrollPane);
+
+    // This is adding the typePanel and reviewPanel to the centerPanel.
+    centerPanel.add(typePanel, BorderLayout.PAGE_START);
+    centerPanel.add(reviewPanel, BorderLayout.PAGE_END);
+
+    // This is adding the centerPanel to the totalPanel and the totalPanel to the frame.
+    totalPanel.add(centerPanel, BorderLayout.CENTER);
+    this.add(totalPanel, BorderLayout.CENTER);
+  }
+
+  public static void createAndShowGUI() {
+    MobileDeviceFormV2 msw = new MobileDeviceFormV2("Mobile Device Form V2");
+    msw.addComponents();
+    msw.setFrameFeatures();
+  }
+
+  public static void main(String[] args) {
+    SwingUtilities.invokeLater(
+      new Runnable() {
+        public void run() {
+          createAndShowGUI();
+        }
+      }
+    );
+  }
+}
--- papakang/patharanan/lab9/MobileDeviceFormV7.java
+++ papakang/patharanan/lab9/MobileDeviceFormV7.java
@@ -1,4 +1,6 @@
-/**
+/**This program extends MobileDeviceFormV6 and implements  interface ActionListener
+ * Implement the method addListeners() which was called in createAndShowGUI()
+ *  it adds listeners to OK button and Cancel button.
  * @Auther Patharanan Papakang
  * @Version 1,11 Mar,2022
  */
@@ -9,61 +11,66 @@ import javax.swing.*;
 import javax.swing.JOptionPane;
 import papakang.patharanan.lab8.MobileDeviceFormV6;
 
-public class MobileDeviceFormV7 extends MobileDeviceFormV6 {
-
-  protected ButtonHandler buttonListener; //listener object
+public class MobileDeviceFormV7
+  extends MobileDeviceFormV6
+  implements ActionListener {
 
   public MobileDeviceFormV7(String titel) {
     super(titel);
-    buttonListener = new ButtonHandler();
   }
 
-  class ButtonHandler implements ActionListener {
+  /**
+   * Override the method actionPerformed()
+   * When the OK button is pressed a dialog is displayed showing all the information that is currently input. This includes Brand name, Model name, Color, Weight, Price, Mobile OS, Type, Review, and Features.
+   * For components that have multiple selections
+   * When the Cancel button is pressed, all the text fields and text area will be empty.
+   * The Mobile OS will be reset back to Android. Type resets Phone. And all item in Features will be deselected
+   */
+  @Override
+  public void actionPerformed(ActionEvent e) { //e is event object
+    Object srcObj = e.getSource();
+    androidJRB.setActionCommand("Androind"); //set action command for android button.
+    iOSJRB.setActionCommand("ios"); //set action command for ios button.
 
-    @Override
-    public void actionPerformed(ActionEvent e) { //e is event object
-      Object srcObj = e.getSource();
-      androidJRB.setActionCommand("Androind");
-      iOSJRB.setActionCommand("ios");
+    if (srcObj == cancelButton) {
+      txtBrand.setText("");
+      txtModel.setText("");
+      txtColor.setText("");
+      txtWeight.setText("");
+      txtPrice.setText("");
+      androidJRB.setSelected(true); //Mobile OS be reset back to Android
+      typeComboBox.setSelectedItem("Phone"); //Type resets Phone
+      reviewTxtArea.setText("");
+      featuresList.clearSelection();
+    } else if (srcObj == okButton) {
+      String[] total = {
+        "Brand Name : " + txtBrand.getText(),
+        "Model Name : " + txtModel.getText(),
+        "Color : " + txtColor.getText(),
+        "Weight(kg.) : " + txtWeight.getText(),
+        "Price(Baht) : " + txtPrice.getText(),
+        "Mobile OS : " + mobileGrp.getSelection().getActionCommand(),
+        "Type : " + typeComboBox.getSelectedItem(),
+        "Review : " + reviewTxtArea.getText(),
+        "Features : " +
+        featuresList
+          .getSelectedValuesList()
+          .toString()
+          .replace("[", "")
+          .replace("]", ""),
+      };
 
-      if (srcObj == cancelButton) {
-        txtBrand.setText("");
-        txtModel.setText("");
-        txtColor.setText("");
-        txtWeight.setText("");
-        txtPrice.setText("");
-        androidJRB.setSelected(true);
-        typeComboBox.setSelectedItem("Phone");
-        reviewTxtArea.setText("");
-        featuresList.clearSelection();
-      } else if (srcObj == okButton) {
-        String[] total = {
-          "Brand Name : " + txtBrand.getText(),
-          "Model Name : " + txtModel.getText(),
-          "Color : " + txtColor.getText(),
-          "Weight(kg.) : " + txtWeight.getText(),
-          "Price(Baht) : " + txtPrice.getText(),
-          "Mobile OS : " + mobileGrp.getSelection().getActionCommand(),
-          "Type : " + typeComboBox.getSelectedItem(),
-          "Review : " + reviewTxtArea.getText(),
-          "Features : " +
-          featuresList
-            .getSelectedValuesList()
-            .toString()
-            .replace("[", "")
-            .replace("]", ""),
-        };
-
-        JOptionPane.showMessageDialog(null, total);
-        System.exit(0);
-      }
+      JOptionPane.showMessageDialog(null, total);
+      System.exit(0);
     }
   }
 
   public void addListeners() {
     //specify listener object to listen to source object
-    cancelButton.addActionListener(buttonListener);
-    okButton.addActionListener(buttonListener);
+    androidJRB.setActionCommand("Androind");
+    iOSJRB.setActionCommand("ios");
+    cancelButton.addActionListener(this);
+    okButton.addActionListener(this);
   }
 
   public static void createAndShowGUI() {
